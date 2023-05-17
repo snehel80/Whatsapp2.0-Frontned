ae8da7bebd87ccc644506d36e0b214e5baebd44a
snehel <slsf3939@gmail.com>
2023-05-17 23:49:02 +0530

    fourth commit

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -11,7 +11,7 @@ const AccountProvider = ({ children }) => {
     const socket = useRef();
 
     useEffect(()=>{
-        socket.current = io('ws://localhost:9000');
+        socket.current = io('https://whatsapp20-socket-production.up.railway.app/');
     },[])
     
     return (
